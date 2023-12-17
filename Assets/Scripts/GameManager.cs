67109edb6234f018566694c9cf61927ce8e68755
ctcticxh <3574736597@qq.com>
2023-12-17 15:27:52 +0800

    添加落子和落子提示

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject blackPiece;
     public GameObject whitePiece;
+    public GameObject border;
     public float xDis;
     public float yDis;
+    public bool isMyTurn;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +19,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isMyTurn)
+        {
+            DisplayBorder();
+        }
         if (Input.GetMouseButtonDown(0))
         {
             GeneratePiece();
@@ -35,13 +42,22 @@ public class GameManager : MonoBehaviour
     //    }
     //}
 
-    void GeneratePiece()
+    Vector3 ClosestPoint()
     {
         Vector3 mousePos = TransScreenToWorld(Input.mousePosition);
-        Vector3 piecePos = new Vector3(Mathf.Round(mousePos.x / xDis) * xDis,
-            Mathf.Round(mousePos.y / yDis) * yDis, 0);
+        float xCo = Mathf.Round(mousePos.x / xDis), yCo = Mathf.Round(mousePos.y / yDis);
+        Debug.Log("x = " + xCo + " yCo = " + yCo);
+        Vector3 pointPos = new Vector3(xCo * xDis, yCo * yDis, 0);
+        if (xCo > 7 || xCo < -7 || yCo > 7 || yCo < -7) pointPos.z = -1;
+        return pointPos;
+    }
+
+    void GeneratePiece()
+    {
+        Vector3 pos = ClosestPoint();
+        if (pos.z < 0) return;
         GameObject piece = Instantiate(whitePiece);
-        piece.transform.position = new Vector3(piecePos.x, piecePos.y, 0);
+        piece.transform.position = pos;
     }
 
     Vector3 TransScreenToWorld(Vector3 pos)
@@ -49,4 +65,14 @@ public class GameManager : MonoBehaviour
         Vector3 t = Camera.main.ScreenToWorldPoint(pos);
         return new Vector3(t.x, t.y, 0);
     }
+
+    void DisplayBorder()
+    {
+        Vector3 pos = ClosestPoint();
+        if (pos.z < 0) return;
+        GameObject bdr = Instantiate(border);
+        bdr.transform.position = pos;
+        Destroy(bdr, 0.05f);
+    }
+
 }
