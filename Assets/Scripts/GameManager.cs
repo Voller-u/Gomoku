d235853ed39b8babb3160ad8b7c4effdbaab9fbb
ctcticxh <3574736597@qq.com>
2023-12-18 23:04:29 +0800

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Text.RegularExpressions;
 using UnityEngine;
 
@@ -15,6 +16,25 @@ public class GameManager : MonoBehaviour
     public bool isBlack;
 
     public static int[,] chessBoard = new int[15, 15];
+    public enum ChessType
+    {
+        STwo,//冲二
+        SThree,//冲三
+        SFour,//冲四
+        Two,//活二
+        Three,//活三
+        Four,//活四
+        Five,//五成
+    }
+    public enum DifficultyLevel { Easy,Middle,Hard,Expert};
+
+    public static List<List<string>> blackChessTypes = new List<List<string>>();
+    public static List<List<string>> whiteChessTypes = new List<List<string>>();
+
+    //白棋价值表
+    public Dictionary<string, int> whiteEvalTable = new Dictionary<string, int>();
+    //黑棋价值表
+    public Dictionary<string, int> blackEvalTable = new Dictionary<string, int>();
 
     // Start is called before the first frame update
     void Start()
@@ -143,6 +163,135 @@ public class GameManager : MonoBehaviour
         while (x + r <= 14 && y + r <= 14 && chessBoard[x + r, y + r] == c) r++;
         if (l + r - 1 >= 5) return true;
         return false;
+    }
+
+    void InitEvalTable(DifficultyLevel level)
+    {
+        //根据不同难度初始化价值表
+        switch (level)
+        {
+            case DifficultyLevel.Easy:
+                {
+                    break;
+                }
+            case DifficultyLevel.Middle:
+                {
+                    break;
+                }
+            case DifficultyLevel.Hard:
+                {
+                    blackChessTypes[(int)ChessType.STwo].Add("21100");//冲二
+                    
+                    blackChessTypes[(int)ChessType.SThree].Add("21110");//冲三
+                    blackChessTypes[(int)ChessType.SFour].Add("122220");//冲四
+                    blackChessTypes[(int)ChessType.Two].Add("01100");//活二
+                    blackChessTypes[(int)ChessType.Three].Add("01110");//活三
+                    blackChessTypes[(int)ChessType.Four].Add("011110");//活四
+                    blackChessTypes[(int)ChessType.Five].Add("11111");//五成
+                    for (int i = 0; i <= 6; i++)
+                        for (int j = 0; j < blackChessTypes[i].Count; j++)
+                            whiteChessTypes[i].Add(blackChessTypes[i][j].
+                                Replace('2','-').Replace('1','2').Replace('2','1'));
+                    break;
+                }
+            case DifficultyLevel.Expert:
+                {
+                    //whiteEvalTable.Clear();
+                    //blackEvalTable.Clear();
+                    //blackEvalTable.Add( "11111", 10000 );
+                    
+                    break;
+                }
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 分析落子后的棋局并返回棋形计数数组
+    /// </summary>
+    /// <param name="board">落子后的虚拟棋盘</param>
+    /// <param name="x">落子的x坐标</param>
+    /// <param name="y">落子的y坐标</param>
+    /// <param name="whiteCnt">白棋棋形计数数组</param>
+    /// <param name="blackCnt">黑棋棋形计数数组</param>
+    /// <param name="isBlack">布尔变量，判断此时是为哪一方计算分数</param>
+    void Analyse(int[,] board,int x,int y, int[] whiteCnt, int[] blackCnt,bool isBlack)
+    {
+        AnalyseHorizontal(board, x, y, whiteCnt,blackCnt,isBlack);
+        AnalyseVertical(board, x, y, whiteCnt, blackCnt, isBlack);
+        AnalyseLeft(board, x, y, whiteCnt, blackCnt, isBlack);
+        AnalyseRight(board, x, y, whiteCnt, blackCnt, isBlack);
+    }
+
+    void TypeCount(string s,int[] blackCnt, int[] whiteCnt)
+    {
+        for (int i = 0; i < blackChessTypes.Count; i++)
+        {
+            for(int j = 0; j < blackChessTypes[i].Count;j++)
+                blackCnt[i] += Regex.Matches(s, blackChessTypes[i][j]).Count;
+        }
+        for (int i = 0; i < whiteChessTypes.Count; i++)
+        {
+            for (int j = 0; j < whiteChessTypes[i].Count; j++)
+                whiteCnt[i] += Regex.Matches(s, whiteChessTypes[i][j]).Count;
+        }
+    }
+
+    void AnalyseHorizontal(int[,] board, int x, int y, int[] whiteCnt, int[] blackCnt, bool isBlack)
+    {
+        string line = "";
+        for (int i = 0; i < 14; i++) line += board[i, y].ToString();
+        TypeCount(line, blackCnt, whiteCnt);
+    }
+    void AnalyseVertical(int[,] board, int x, int y, int[] whiteCnt, int[] blackCnt, bool isBlack)
+    {
+        string colume = "";
+        for (int i = 0; i < 14; i++) colume += board[x, i].ToString();
+        TypeCount(colume, blackCnt, whiteCnt);
+    }
+    void AnalyseLeft(int[,] board, int x, int y, int[] whiteCnt, int[] blackCnt, bool isBlack)
+    {
+        string leftSlash = "";
+        if (x + y <= 14)
+        {//在下半段
+            for(int i = 0; y >= 0; y--, i++)
+            {
+                leftSlash += board[i, y].ToString();
+            }
+        }
+        else
+        {//在上半段
+            for(int i = 14; y <= 14; y++, i--)
+            {
+                leftSlash += board[i, y].ToString();
+            }
+        }
+        TypeCount(leftSlash, blackCnt, whiteCnt);
+    }
+    void AnalyseRight(int[,] board, int x, int y, int[] whiteCnt, int[] blackCnt, bool isBlack)
+    {
+        string rightSlash = "";
+        if(y-x <= 0)
+        {//在下半段
+            for(int i = 0; x <= 1; x++, i++)
+            {
+                rightSlash += board[x, i].ToString();
+            }
+        }
+        else
+        {
+            for(int i = 14; x >= 0; x--, i--)
+            {
+                rightSlash += board[x, i].ToString();
+            }
+        }
+        TypeCount(rightSlash, blackCnt, whiteCnt);
+    }
+
+    int AlphaBeta()
+    {
+
     }
 
 }
